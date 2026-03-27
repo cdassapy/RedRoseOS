c24b8bab445845215304c83f0505a9de363e0cbe
Assapy <assapy.redrose@proton.me>
2026-03-27 21:33:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let downloadCount = 0; 
-
+let downloadCount = 0;
 const downloadBtn = document.getElementById('download-btn');
 const countDisplay = document.getElementById('download-count');
 
 downloadBtn.addEventListener('click', function(e) {
     e.preventDefault(); 
-    
     downloadCount++;
     countDisplay.innerText = downloadCount;
-    
 });
