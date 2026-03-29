581ac4b5cfc45038f4ddf859f329f69777946b63
Assapy <assapy.redrose@proton.me>
2026-03-29 23:17:39 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let downloadCount = 0;
-const downloadBtn = document.getElementById('download-btn');
-const countDisplay = document.getElementById('download-count');
-
-downloadBtn.addEventListener('click', function(e) {
-    e.preventDefault(); 
-    downloadCount++;
-    countDisplay.innerText = downloadCount;
-});
